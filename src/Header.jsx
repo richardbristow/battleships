73a241aaabf785fe219d35841cdf93b880a90920
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-06-07 00:12:46 +0100

    added icon to header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,22 @@
 import styled from 'styled-components/macro';
+import crosshairRed from './assets/crosshair-red.png';
 
-const StyledHeader = styled.h1`
-  text-align: center;
+const StyledHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    margin-left: 5px;
+    height: 2.5em;
+  }
 `;
 
-const Header = () => <StyledHeader>BATTLESHIPS</StyledHeader>;
+const Header = () => (
+  <StyledHeader>
+    <h1>BATTLESHIPS</h1>
+    <img src={crosshairRed} alt="header crosshair" />
+  </StyledHeader>
+);
 
 export default Header;
