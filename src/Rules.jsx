64430e6165694939e2fa86a6f99a00e201f5e1fd
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-06-06 23:10:27 +0100

    updated rules page

--- src/Rules.jsx
+++ src/Rules.jsx
@@ -1,6 +1,10 @@
 const Rules = () => (
   <div>
     <p>Rules are coming soon!</p>
+    <p>
+      A game of battleships is won by the player who sinks all their opponents
+      battleships first.
+    </p>
   </div>
 );
 
