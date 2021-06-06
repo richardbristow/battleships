d1e375ef5512ef7d72edaeaa3e309e0e17bd6bbe
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-06-06 15:26:38 +0100

    Initial commit of Container Component

---
+++ src/Container.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components/macro';
+
+const StyledContainer = styled.div`
+  fieldset {
+    border: 4px solid white;
+    padding: 20px;
+  }
+
+  legend {
+    font-size: 0.8em;
+    width: 0%;
+    padding: 0px 4px;
+    margin: 0px;
+  }
+`;
+
+const Container = ({ children, className, title }) => (
+  <StyledContainer className={className}>
+    <fieldset>
+      {title && <legend>{title}</legend>}
+      {children}
+    </fieldset>
+  </StyledContainer>
+);
+
+export default Container;
