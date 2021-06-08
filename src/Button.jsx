43f767a0b5b7dd424c8db49e82ebd753991c9ab9
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-06-08 19:01:01 +0100

    updated Button to allow pixel art

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,7 +1,47 @@
-const Button = ({ label, type, onClick }) => (
-  <button onClick={onClick} type="button" className={`nes-btn ${type}`}>
-    {label}
-  </button>
-);
+import PropTypes from 'prop-types';
+
+import PixelArt from './PixelArt';
+
+const Button = ({
+  label,
+  type,
+  onClick,
+  pixelArt,
+  pixelArtHeight,
+  pixelArtOverrideTheme,
+}) => {
+  const buttonClass = type === 'is-normal' ? 'nes-btn' : `nes-btn ${type}`;
+
+  return (
+    <button onClick={onClick} type="button" className={type && buttonClass}>
+      {pixelArt && (
+        <PixelArt
+          name={pixelArt}
+          overrideTheme={pixelArtOverrideTheme}
+          height={pixelArtHeight}
+        />
+      )}
+      {label}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  label: null,
+  type: null,
+  onClick: null,
+  pixelArt: null,
+  pixelArtHeight: '50px',
+  pixelArtOverrideTheme: null,
+};
+
+Button.propTypes = {
+  label: PropTypes.string,
+  type: PropTypes.string,
+  onClick: PropTypes.func,
+  pixelArt: PropTypes.string,
+  pixelArtHeight: PropTypes.string,
+  pixelArtOverrideTheme: PropTypes.oneOf(['light', 'dark']),
+};
 
 export default Button;
