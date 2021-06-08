fbfa4d4ab426ae26f643fb7b8410299faf9ca96f
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-06-08 14:13:37 +0100

    LinkCustom uses theme

--- src/LinkCustom.jsx
+++ src/LinkCustom.jsx
@@ -1,16 +1,22 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components/macro';
+import theme from 'styled-theming';
+
+const linkColor = theme('mode', {
+  light: '#212529',
+  dark: '#fff',
+});
 
 const StyledLink = styled(Link)`
-  ${({ $noDecoration }) => $noDecoration && 'color: white;'}
+  color: ${({ $noDecoration }) => $noDecoration && linkColor};
 
   &:focus,
   &:hover,
   &:visited,
   &:link,
   &:active {
-    ${({ $noDecoration }) =>
-      $noDecoration && 'color: white; text-decoration: none;'}
+    ${({ $noDecoration }) => $noDecoration && 'text-decoration: none;'}
+    color: ${({ $noDecoration }) => $noDecoration && linkColor};
   }
 `;
 
