200eaa901bfbf9b90d5d0fb3d0fdd51c78bb5a76
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-06-08 13:36:05 +0100

    GlobalStyle uses theme

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,11 +1,22 @@
 import { createGlobalStyle } from 'styled-components/macro';
+import theme from 'styled-theming';
 import 'nes.css/css/nes.min.css';
+
+const globalBodyBackgroundColor = theme('mode', {
+  light: '#fff',
+  dark: '#212529',
+});
+
+const globalTextColor = theme('mode', {
+  light: '#212529',
+  dark: '#fff',
+});
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background-color: #212529;
+    background-color: ${globalBodyBackgroundColor};
     font-family: 'Press Start 2P', cursive;
-    color: white;
+    color: ${globalTextColor};
   }
 
   * {
