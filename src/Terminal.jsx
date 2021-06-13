9f7462b22f8ef69f59c573ad87f9662caff63378
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-06-13 13:47:22 +0100

    addad terminal component

---
+++ src/Terminal.jsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components/macro';
+
+import './keyframes.css';
+
+const StyledTerminal = styled.div`
+  &:before {
+    content: ' ';
+    display: block;
+    position: absolute;
+    top: 12px;
+    left: 4px;
+    bottom: 4px;
+    right: 4px;
+    background: linear-gradient(
+        rgba(18, 16, 16, 0) 50%,
+        rgba(0, 0, 0, 0.25) 50%
+      ),
+      linear-gradient(
+        90deg,
+        rgba(255, 0, 0, 0.06),
+        rgba(0, 255, 0, 0.02),
+        rgba(0, 0, 255, 0.06)
+      );
+    background-size: 100% 2px, 3px 100%;
+    z-index: 2;
+    pointer-events: none;
+  }
+`;
+
+const StyledScanline = styled.div`
+  width: 100%;
+  height: 100px;
+  z-index: 8;
+  background: linear-gradient(
+    0deg,
+    rgba(0, 0, 0, 0) 0%,
+    rgba(255, 255, 255, 0.2) 10%,
+    rgba(0, 0, 0, 0.1) 100%
+  );
+  opacity: 0.1;
+  position: absolute;
+  bottom: 100%;
+  animation: scanline 8s linear infinite;
+`;
+
+const Terminal = ({ children, scanline }) => (
+  <StyledTerminal>
+    {scanline && <StyledScanline />}
+    {children}
+  </StyledTerminal>
+);
+
+export default Terminal;
