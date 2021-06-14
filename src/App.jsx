1f39704a95120fa78a9138b8c4b73657a9d02542
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-06-14 12:20:01 +0100

    moved Game to separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,27 @@
-import styled from 'styled-components/macro';
-import { useResizeDetector } from 'react-resize-detector';
 import { Switch, Route } from 'react-router-dom';
 
 import Header from './Header';
-import Grid from './Grid';
-import CommandPanel from './CommandPanel';
 import Rules from './Rules';
 import NavBar from './NavBar';
 import NoRoute from './NoRoute';
+import Game from './Game';
 
-const StyledGameWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 20px;
-`;
-
-const StyledGridWrapper = styled.div`
-  flex: 78%;
-  height: 100%;
-  display: flex;
-  gap: 20px;
-`;
-
-const App = () => {
-  const { width, height, ref } = useResizeDetector();
-
-  return (
-    <>
-      <Header />
-      <NavBar />
-      <Switch>
-        <Route exact path="/">
-          <StyledGameWrapper>
-            <CommandPanel shipSquareDimensions={{ width, height }} />
-            <StyledGridWrapper>
-              <Grid type="Player" ref={ref} />
-              <Grid type="Opponent" />
-            </StyledGridWrapper>
-          </StyledGameWrapper>
-        </Route>
-        <Route path="/rules">
-          <Rules />
-        </Route>
-        <Route path="*">
-          <NoRoute />
-        </Route>
-      </Switch>
-    </>
-  );
-};
+const App = () => (
+  <>
+    <Header />
+    <NavBar />
+    <Switch>
+      <Route exact path="/">
+        <Game />
+      </Route>
+      <Route path="/rules">
+        <Rules />
+      </Route>
+      <Route path="*">
+        <NoRoute />
+      </Route>
+    </Switch>
+  </>
+);
 
 export default App;
---
+++ src/Game.jsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components/macro';
+import { useResizeDetector } from 'react-resize-detector';
+
+import Grid from './Grid';
+import CommandPanel from './CommandPanel';
+
+const StyledGameWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+`;
+
+const StyledGridWrapper = styled.div`
+  flex: 78%;
+  height: 100%;
+  display: flex;
+  gap: 20px;
+`;
+
+const Game = () => {
+  const { width, height, ref } = useResizeDetector();
+
+  return (
+    <StyledGameWrapper>
+      <CommandPanel shipSquareDimensions={{ width, height }} />
+      <StyledGridWrapper>
+        <Grid type="Player" ref={ref} />
+        <Grid type="Opponent" />
+      </StyledGridWrapper>
+    </StyledGameWrapper>
+  );
+};
+
+export default Game;
