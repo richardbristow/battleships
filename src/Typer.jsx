49c57cd340378dda4e59c3d329872d8ccc150ed6
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-08-20 19:16:41 +0100

    refactor stringTyper and Typer component

--- src/Typer.jsx
+++ src/Typer.jsx
@@ -1,49 +1,32 @@
 import React, { useEffect, useState, useRef, useCallback } from 'react';
 
-const TextTyper = ({
-  setChildrenIndex,
-  characterDelay,
-  textBlockDelay,
-  children,
-}) => {
+const TextTyper = ({ characterDelay, children, blockDelayTimer }) => {
   const [text, setText] = useState('');
   const [currentCharIndex, setCurrentCharIndex] = useState(0);
   const {
     props: { children: childToType },
   } = children;
   const charDelayTimerRef = useRef(null);
-  const blockDelayTimerRef = useRef(null);
 
-  const blockDelayTimer = useCallback(() => {
-    blockDelayTimerRef.current = setTimeout(() => {
-      setChildrenIndex((prevChildrenIndex) => prevChildrenIndex + 1);
-    }, textBlockDelay);
-  }, [setChildrenIndex, textBlockDelay]);
-
-  const stringTyper = useCallback(() => {
-    if (currentCharIndex < childToType.length) {
-      charDelayTimerRef.current = setTimeout(() => {
-        setText((prevText) => prevText + childToType[currentCharIndex]);
-        setCurrentCharIndex(currentCharIndex + 1);
-      }, characterDelay);
-    } else {
-      blockDelayTimer();
-    }
-  }, [blockDelayTimer, characterDelay, childToType, currentCharIndex]);
+  const stringTyper = useCallback(
+    (stringToType) => {
+      if (currentCharIndex < stringToType.length) {
+        charDelayTimerRef.current = setTimeout(() => {
+          setText((prevText) => prevText + stringToType[currentCharIndex]);
+          setCurrentCharIndex(currentCharIndex + 1);
+        }, characterDelay);
+      } else {
+        blockDelayTimer();
+      }
+    },
+    [blockDelayTimer, characterDelay, currentCharIndex],
+  );
 
   useEffect(() => {
-    if (typeof childToType === 'string') {
-      stringTyper();
-    } else if (React.isValidElement(childToType)) {
-      setText(childToType);
-      blockDelayTimer();
-    }
+    stringTyper(childToType);
 
-    return () => {
-      clearTimeout(charDelayTimerRef.current);
-      clearTimeout(blockDelayTimerRef.current);
-    };
-  }, [blockDelayTimer, childToType, stringTyper]);
+    return () => clearTimeout(charDelayTimerRef.current);
+  }, [childToType, stringTyper]);
 
   return React.cloneElement(children, {
     children: text,
@@ -52,16 +35,38 @@ const TextTyper = ({
 
 const Typer = ({ characterDelay, textBlockDelay, children }) => {
   const [childrenIndex, setChildrenIndex] = useState(0);
+  const blockDelayTimerRef = useRef(null);
 
-  const clonedArray = React.Children.map(children, (child) => (
-    <TextTyper
-      setChildrenIndex={setChildrenIndex}
-      characterDelay={characterDelay}
-      textBlockDelay={textBlockDelay}
-    >
-      {child}
-    </TextTyper>
-  ));
+  const blockDelayTimer = useCallback(() => {
+    blockDelayTimerRef.current = setTimeout(() => {
+      setChildrenIndex((prevChildrenIndex) => prevChildrenIndex + 1);
+    }, textBlockDelay);
+  }, [setChildrenIndex, textBlockDelay]);
+
+  const clonedArray = React.Children.map(children, (child) => {
+    const {
+      props: { children: childToType },
+    } = child;
+
+    if (typeof childToType === 'string') {
+      return (
+        <TextTyper
+          setChildrenIndex={setChildrenIndex}
+          characterDelay={characterDelay}
+          textBlockDelay={textBlockDelay}
+          blockDelayTimer={blockDelayTimer}
+        >
+          {child}
+        </TextTyper>
+      );
+    } else if (React.isValidElement(childToType)) {
+      return child;
+    } else if (Array.isArray(childToType)) {
+      console.log('array');
+    } else {
+      return null;
+    }
+  });
 
   return clonedArray.slice(0, childrenIndex + 1);
 };
