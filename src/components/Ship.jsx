0d8bea8cfc2e27d8a13eb259d3b7a25f8b823409
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-08-27 13:21:32 +0100

    refactored StyledShipSquare

--- src/components/Ship.jsx
+++ src/components/Ship.jsx
@@ -14,57 +14,45 @@ const borderColor = theme('mode', {
 const StyledShip = styled.div`
   display: flex;
   flex-direction: ${({ isVertical }) => (isVertical ? 'column' : 'row')};
-  border-top: 4px solid ${borderColor};
   cursor: url(${draggerBlackCursor}), move;
   opacity: ${({ isDragging }) => (isDragging ? '0.5' : '1')};
-
-  div {
-    height: ${({ shipSquareDimensions }) =>
-      shipSquareDimensions && `${(shipSquareDimensions.height - 4) / 10}px`};
-    width: ${({ shipSquareDimensions }) =>
-      shipSquareDimensions && `${(shipSquareDimensions.width - 4) / 10}px`};
-    background-color: grey;
-
-    ${({ isVertical }) =>
-      isVertical
-        ? css`
-            border-left: 4px solid ${borderColor};
-            border-right: 4px solid ${borderColor};
-          `
-        : css`
-            border-bottom: 4px solid ${borderColor};
-            &:first-child {
-              border-left: 4px solid ${borderColor};
-            }
-          `}
-
-    &:last-child {
-      ${({ isVertical }) =>
-        isVertical
-          ? css`
-              border-bottom: 4px solid ${borderColor};
-            `
-          : css`
-              border-right: 4px solid ${borderColor};
-            `}
-    }
-  }
 `;
 
 const StyledShipSquare = styled.div`
+  height: ${({ shipSquareDimensions }) =>
+    shipSquareDimensions && `${shipSquareDimensions.height / 10}px`};
+  width: ${({ shipSquareDimensions }) =>
+    shipSquareDimensions && `${shipSquareDimensions.width / 10}px`};
+  background-color: grey;
+
   ${({ isVertical }) =>
     isVertical
       ? css`
-          border-bottom: 2px solid ${borderColor};
+          &:first-child {
+            border-top: 2px solid ${borderColor};
+          }
+          &:last-child {
+            border-bottom: 2px solid ${borderColor};
+          }
+          border-width: 1px 2px 1px 2px;
+          border-style: solid;
+          border-color: ${borderColor};
         `
       : css`
-          border-right: 2px solid ${borderColor};
+          &:first-child {
+            border-left: 2px solid ${borderColor};
+          }
+          &:last-child {
+            border-right: 2px solid ${borderColor};
+          }
+          border-width: 2px 1px 2px 1px;
+          border-style: solid;
+          border-color: ${borderColor};
         `}
 `;
 
 const Ship = ({ shipName, size, shipSquareDimensions }) => {
   const [isVertical, setIsVertical] = useState(true);
-
   const [{ isDragging }, drag] = useDrag(
     () => ({
       type: 'ship',
@@ -82,10 +70,13 @@ const Ship = ({ shipName, size, shipSquareDimensions }) => {
       isVertical={isVertical}
       isDragging={isDragging}
       ref={drag}
-      shipSquareDimensions={shipSquareDimensions}
     >
       {[...Array(size).keys()].map((id) => (
-        <StyledShipSquare isVertical={isVertical} key={`shipSquare-${id}`} />
+        <StyledShipSquare
+          shipSquareDimensions={shipSquareDimensions}
+          isVertical={isVertical}
+          key={`shipSquare-${id}`}
+        />
       ))}
     </StyledShip>
   );
