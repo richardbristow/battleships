95ae4509bf19196dbca5dcdc134174cad38e6e03
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-09-05 15:45:54 +0100

    added padding-bottom border fix back

--- src/components/GridSquare.jsx
+++ src/components/GridSquare.jsx
@@ -4,7 +4,7 @@ import { useDrop } from 'react-dnd';
 const StyledGridSquare = styled.div`
   background-color: lightskyblue;
   flex: 10%;
-  padding-bottom: 10%;
+  padding-bottom: calc(10% - 2px);
   border-bottom: 2px solid #212529;
   border-right: 2px solid #212529;
 `;
