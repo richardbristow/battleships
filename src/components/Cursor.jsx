93e0e0a3ef0d720296531f8adb00acf5c6b01885
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-09-06 17:57:22 +0100

    initial commit of Cursor component

---
+++ src/components/Cursor.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components/macro';
+
+const StyledCursor = styled.span`
+  display: inline-block;
+  width: 14px;
+  height: 20px;
+  background-color: #fff;
+  color: #fff;
+`;
+
+const Cursor = () => <StyledCursor />;
+
+export default Cursor;
