b6488e1b9836cdaa011e899903c4a55a917ed86b
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-09-07 11:50:53 +0100

    added terminalText effect to Cursor

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,13 +1,30 @@
-import styled from 'styled-components/macro';
+import styled, { css } from 'styled-components/macro';
+import PropTypes from 'prop-types';
+
+import '../keyframes.css';
 
 const StyledCursor = styled.span`
   display: inline-block;
   width: 14px;
   height: 20px;
   background-color: #fff;
-  color: #fff;
+  ${({ terminalText }) =>
+    terminalText &&
+    css`
+      animation: boxShadow 2s infinite;
+    `}
 `;
 
-const Cursor = () => <StyledCursor />;
+const Cursor = ({ terminalText }) => (
+  <StyledCursor terminalText={terminalText} />
+);
+
+Cursor.defaultProps = {
+  terminalText: false,
+};
+
+Cursor.propTypes = {
+  terminalText: PropTypes.bool,
+};
 
 export default Cursor;
